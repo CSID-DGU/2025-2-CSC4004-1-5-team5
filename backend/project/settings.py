4dc1ec685e69fcb71796686278b5944dda93d586
Yeonhee-Ha <78415838+Yeonhee-Ha@users.noreply.github.com>
2025-11-11 08:01:52 +0900

    🧱 [#32] chore : 정적 파일 및 Swagger 설정 수정

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -137,8 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'          # collectstatic 결과 저장용
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']  # 개발용
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
