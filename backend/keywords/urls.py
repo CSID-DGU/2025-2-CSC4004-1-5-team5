a3138d8b81fa5d050578908a3906c1a327b49ff0
Yeonhee-Ha <78415838+Yeonhee-Ha@users.noreply.github.com>
2025-11-21 23:02:49 +0900

    🏗️ [#58] Chore: keywords 앱 파일 구조 정리

--- backend/keywords/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import KeywordViewSet
-
-app_name = "keywords"
-router = DefaultRouter()
-router.register(r"recordings", KeywordViewSet, basename="keywords")
-
-urlpatterns = router.urls
